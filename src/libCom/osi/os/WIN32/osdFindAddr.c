27b5db20fbf3c9fb936a48f1a2be2613ccbe3398
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-07-27 18:28:01 +0100

    Implement osdFindAddr on WIN32 for stack backtrace symbols

---
+++ src/libCom/osi/os/WIN32/osdFindAddr.c
@@ -0,0 +1,52 @@
+#include <windows.h>
+#include <dbghelp.h>
+#define epicsExportSharedSymbols
+#include "epicsStackTracePvt.h"
+#include "epicsStackTrace.h"
+
+int epicsFindAddr(void *addr, epicsSymbol *sym_p)
+{
+	static int first_call = 1;
+	static HANDLE process = NULL;
+	DWORD64 displacement = 0;
+	SYMBOL_INFO *symbol;
+	IMAGEHLP_LINE64 line;
+	if (first_call)
+	{
+		SymSetOptions(SYMOPT_LOAD_LINES | SYMOPT_DEFERRED_LOADS);
+		process = GetCurrentProcess();
+		SymInitialize(process, NULL, TRUE);
+		first_call = 0;
+	}
+	line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+	symbol = (SYMBOL_INFO *)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
+	symbol->MaxNameLen = 255;
+	symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+	if (!SymFromAddr(process, addr, &displacement, symbol))
+	{
+		sym_p->s_nam = 0;
+		sym_p->s_val = 0;
+		sym_p->f_nam = 0;
+		free(symbol);
+		return -1;
+	}
+	sym_p->s_nam = strdup(symbol->Name);
+	sym_p->s_val = symbol->Address;
+	if (SymGetLineFromAddr64(process, addr, &displacement, &line))
+	{
+		sym_p->f_nam = strdup(line.FileName);
+	}
+	else
+	{
+		sym_p->f_nam = 0;
+	}
+	free(symbol);
+	return 0;
+}
+
+int epicsFindAddrGetFeatures(void)
+{
+	return  EPICS_STACKTRACE_LCL_SYMBOLS
+		| EPICS_STACKTRACE_GBL_SYMBOLS
+		| EPICS_STACKTRACE_DYN_SYMBOLS;
+}
