47aaa6502e926f0aa9147b3d53bdfa32ce7f0d5d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-05-24 12:45:49 +0100

    Set names on threads using SetThreadDescription

--- modules/libcom/src/osi/os/WIN32/setThreadName.cpp
+++ modules/libcom/src/osi/os/WIN32/setThreadName.cpp
@@ -12,16 +12,51 @@
 #define STRICT
 #include <windows.h>
 
+#include <string>
+
 /*
  * Usage: setThreadName (-1, "MainThread");
  */
- 
+
+
+static void setThreadNameVS ( DWORD dwThreadID, LPCSTR szThreadName );
+typedef HRESULT (*setDesc_t)(HANDLE, PCWSTR);
+
 extern "C" void setThreadName ( DWORD dwThreadID, LPCSTR szThreadName )
 {
-	if (szThreadName == NULL || *szThreadName == '\0')
-	{
-		return;
-	}
+    static HMODULE hKernel = LoadLibrary("KernelBase.dll");
+    static setDesc_t pSetDesc = (hKernel != NULL ?
+        (setDesc_t)GetProcAddress(hKernel, "SetThreadDescription") : NULL);
+    if (szThreadName == NULL || *szThreadName == '\0')
+    {
+        return;
+    }
+    if (pSetDesc != NULL)
+    {
+#ifdef THREAD_SET_LIMITED_INFORMATION
+        DWORD thread_access = THREAD_SET_LIMITED_INFORMATION;
+#else
+        DWORD thread_access = THREAD_SET_INFORMATION;
+#endif /* ifdef THREAD_SET_LIMITED_INFORMATION */
+        HANDLE hThread = OpenThread(thread_access, FALSE, dwThreadID);
+        if (hThread != NULL)
+        {
+            const std::string s(szThreadName);
+            const std::wstring ws(s.begin(), s.end());
+            HRESULT hr = (*pSetDesc)(hThread, ws.c_str());
+            CloseHandle(hThread);
+        }
+    }
+    // if SetThreadDescription() was available and we have a recent
+    // visual studio debugger (2017 version 15.6 or higher) attached
+    // then the names will already be defined. However we don't know
+    // this for sure, so also trigger the old exception mechanism.
+    // See https://docs.microsoft.com/en-us/visualstudio/debugger/how-to-set-a-thread-name-in-native-code
+    setThreadNameVS(dwThreadID, szThreadName);
+}
+
+static void setThreadNameVS( DWORD dwThreadID, LPCSTR szThreadName )
+{
 #if _MSC_VER >= 1300 && defined ( _DEBUG )
 // This was copied directly from an MSDN example
 // It sets the thread name by throwing a special exception that is caught by Visual Sudio
@@ -45,13 +80,13 @@ extern "C" void setThreadName ( DWORD dwThreadID, LPCSTR szThreadName )
 #pragma warning(push)
 #pragma warning(disable: 6320 6322)
     __try
-	{
+    {
         RaiseException(MS_VC_EXCEPTION, 0,
-		               sizeof(info) / sizeof(ULONG_PTR),
-					   (ULONG_PTR*)&info);
+                       sizeof(info) / sizeof(ULONG_PTR),
+                       (ULONG_PTR*)&info);
     }
     __except (EXCEPTION_EXECUTE_HANDLER)
-	{
+    {
     }
 #pragma warning(pop)
 #endif
