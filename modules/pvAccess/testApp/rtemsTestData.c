b10b595747f8afb996ff27a5b6dab660de79deff
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-06-06 16:41:11 +0100

    Import upstream base 7.0.9

--- modules/pvAccess/testApp/rtemsTestData.c
+++
@@ -1,2 +0,0 @@
-/* no test data */
-const void* epicsRtemsFSImage = 0;
--- modules/pva2pva/iocBoot/iocimagedemo/st.cmd
+++
@@ -1,9 +0,0 @@
-#!../../bin/linux-x86_64-debug/softIocPVA
-
-dbLoadRecords("image.db","N=TST:image1")
-dbLoadRecords("table.db","N=TST:table1")
-dbLoadRecords("ntenum.db","P=TST:enum1")
-
-dbLoadGroup("image.json")
-
-iocInit()
